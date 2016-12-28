cd3a42c95e15c7ff585ff709be5d49969a12871f
Steve Wilford <steve@stevewilford.co.uk>
2016-12-28 18:57:22 +0000

    Add test & implementation for reading a single account entry

--- Sources/AccountsFileReader.swift
+++ Sources/AccountsFileReader.swift
@@ -1,7 +1,25 @@
+import Foundation
+
 class AccountsFileReader {
 
   class func read(_ path: URL) -> [String] {
-    return []
+    do {
+      let contents = try String(contentsOf: path)
+      let accounts = contents.components(separatedBy: "\n")
+
+      var entries = [String]()
+
+      if accounts.count == 4 {
+        if let entry = EntryReader.read(accounts) {
+          entries.append(entry)
+        }
+      }
+
+      return entries
+    } catch {
+      print(error)
+      return []
+    }
   }
 
 }
--- Tests/BankOCRTests/AccountsFileReaderTests.swift
+++ Tests/BankOCRTests/AccountsFileReaderTests.swift
@@ -8,4 +8,9 @@ class AccountsFileReaderTests: XCTestCase {
     XCTAssertEqual(0, AccountsFileReader.read(url).count, "Empty input should return an empty array")
   }
 
+  func testSingleEntryOf123456789_GivesArrayWith123456789() {
+    let url = URL(fileURLWithPath: "Account Files/123456789.txt")
+    XCTAssertEqual(["123456789"], AccountsFileReader.read(url), "Single entry should be the only thing in the array")
+  }
+
 }
