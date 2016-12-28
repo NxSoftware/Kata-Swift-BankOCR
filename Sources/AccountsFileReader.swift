8d27de0a7b58ecd12b970dfca93742cf102946a7
Steve Wilford <steve@stevewilford.co.uk>
2016-12-28 21:42:05 +0000

    Extract string parsing into separate function

--- Sources/AccountsFileReader.swift
+++ Sources/AccountsFileReader.swift
@@ -5,27 +5,31 @@ class AccountsFileReader {
   class func read(_ path: URL) -> [String] {
     do {
       let contents = try String(contentsOf: path)
-      let accounts = contents.components(separatedBy: "\n")
-
-      var account = [String]()
-      var entries = [String]()
-      
-      for (lineNumber, line) in accounts.enumerated() {
-        account.append(line)
-
-        if (lineNumber + 1) % 4 == 0 {
-          if let entry = EntryReader.read(account) {
-            entries.append(entry)
-          }
-          account.removeAll()
-        }
-      }
-
-      return entries
+      return parseEntries(from: contents)
     } catch {
       print(error)
       return []
     }
+  }
+  
+  private class func parseEntries(from input: String) -> [String] {
+    let accounts = input.components(separatedBy: "\n")
+    
+    var account = [String]()
+    var entries = [String]()
+    
+    for (lineNumber, line) in accounts.enumerated() {
+      account.append(line)
+
+      if (lineNumber + 1) % 4 == 0 {
+        if let entry = EntryReader.read(account) {
+          entries.append(entry)
+        }
+        account.removeAll()
+      }
+    }
+    
+    return entries
   }
 
 }
