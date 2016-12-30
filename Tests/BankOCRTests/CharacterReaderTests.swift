3a9e6893929795b446bcc5d35eac11cc9cbbb8aa
Steve Wilford <steve@stevewilford.co.uk>
2016-12-30 13:00:28 +0000

    Extract seven segment components

--- Tests/BankOCRTests/CharacterReaderTests.swift
+++ Tests/BankOCRTests/CharacterReaderTests.swift
@@ -7,82 +7,52 @@ class CharacterReaderTests: XCTestCase {
     }
 
     func testZero_returns0() {
-      let digit = [" _ ",
-                   "| |",
-                   "|_|"]
-      let character = CharacterReader.read(digit)!
+      let character = CharacterReader.read(SevenSegmentComponents.zero)!
       XCTAssertEqual(0, character.value, "Given zero, expected 0")
     }
 
     func testOne_returns1() {
-      let digit = ["   ",
-                   "  |",
-                   "  |"]
-      let character = CharacterReader.read(digit)!
+      let character = CharacterReader.read(SevenSegmentComponents.one)!
       XCTAssertEqual(1, character.value, "Given one, expected 1")
     }
 
     func testTwo_returns2() {
-      let digit = [" _ ",
-                   " _|",
-                   "|_ "]
-      let character = CharacterReader.read(digit)!
+      let character = CharacterReader.read(SevenSegmentComponents.two)!
       XCTAssertEqual(2, character.value, "Given two, expected 2")
     }
 
     func testThree_returns3() {
-      let digit = [" _ ",
-                   " _|",
-                   " _|"]
-      let character = CharacterReader.read(digit)!
+      let character = CharacterReader.read(SevenSegmentComponents.three)!
       XCTAssertEqual(3, character.value, "Given three, expected 3")
     }
 
     func testFour_returns4() {
-      let digit = ["   ",
-                   "|_|",
-                   "  |"]
-      let character = CharacterReader.read(digit)!
+      let character = CharacterReader.read(SevenSegmentComponents.four)!
       XCTAssertEqual(4, character.value, "Given four, expected 4")
     }
 
     func testFive_returns5() {
-      let digit = [" _ ",
-                   "|_ ",
-                   " _|"]
-      let character = CharacterReader.read(digit)!
+      let character = CharacterReader.read(SevenSegmentComponents.five)!
       XCTAssertEqual(5, character.value, "Given five, expected 5")
     }
 
     func testSix_returns6() {
-      let digit = [" _ ",
-                   "|_ ",
-                   "|_|"]
-      let character = CharacterReader.read(digit)!
+      let character = CharacterReader.read(SevenSegmentComponents.six)!
       XCTAssertEqual(6, character.value, "Given six, expected 6")
     }
 
     func testSeven_returns7() {
-      let digit = [" _ ",
-                   "  |",
-                   "  |"]
-      let character = CharacterReader.read(digit)!
+      let character = CharacterReader.read(SevenSegmentComponents.seven)!
       XCTAssertEqual(7, character.value, "Given seven, expected 7")
     }
 
     func testEight_returns8() {
-      let digit = [" _ ",
-                   "|_|",
-                   "|_|"]
-      let character = CharacterReader.read(digit)!
+      let character = CharacterReader.read(SevenSegmentComponents.eight)!
       XCTAssertEqual(8, character.value, "Given eight, expected 8")
     }
 
     func testNine_returns9() {
-      let digit = [" _ ",
-                   "|_|",
-                   " _|"]
-      let character = CharacterReader.read(digit)!
+      let character = CharacterReader.read(SevenSegmentComponents.nine)!
       XCTAssertEqual(9, character.value, "Given nine, expected 9")
     }
 
---
+++ Tests/BankOCRTests/SevenSegmentComponents.swift
@@ -0,0 +1,43 @@
+enum SevenSegmentComponents {
+
+  static let zero = [" _ ",
+                     "| |",
+                     "|_|"]
+
+  static let one = ["   ",
+                    "  |",
+                    "  |"]
+
+  static let two = [" _ ",
+                    " _|",
+                    "|_ "]
+
+  static let three = [" _ ",
+                      " _|",
+                      " _|"]
+
+  static let four = ["   ",
+                     "|_|",
+                     "  |"]
+
+  static let five = [" _ ",
+                     "|_ ",
+                     " _|"]
+
+  static let six = [" _ ",
+                    "|_ ",
+                    "|_|"]
+
+  static let seven = [" _ ",
+                      "  |",
+                      "  |"]
+
+  static let eight = [" _ ",
+                      "|_|",
+                      "|_|"]
+
+  static let nine = [" _ ",
+                     "|_|",
+                     " _|"]
+
+}
