18b471ab2841669cabcf0a7e424e16dc18a8369c
Steve Wilford <steve@stevewilford.co.uk>
2016-12-30 11:39:15 +0000

    Extract entry status decoration into it’s own function

--- Sources/AccountsReport.swift
+++ Sources/AccountsReport.swift
@@ -3,12 +3,14 @@ import Foundation
 class AccountsReport {
   
   class func read(_ path: URL) -> [String] {
-    return AccountsFileReader.readEntries(path).map { entry in
-      if entry.checksum == 0 {
-        return entry.value
-      } else {
-        return "\(entry.value) ERR"
-      }
+    return AccountsFileReader.readEntries(path).map(decorate)
+  }
+  
+  private class func decorate(entry: Entry) -> String {
+    if entry.checksum == 0 {
+      return entry.value
+    } else {
+      return "\(entry.value) ERR"
     }
   }
   
